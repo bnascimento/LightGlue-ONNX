c23ea9c5a6b5a05567f7394c8cf143ea1752d30f
Bruno Nascimento <bruno.nascimento@tekever.com>
2025-01-30 21:10:33 +0000

    save correctly

--- onnx_to_tensorrt.py
+++ onnx_to_tensorrt.py
@@ -5,8 +5,6 @@ from polygraphy.backend.trt import (
     CreateConfig,
     EngineFromNetwork,
     NetworkFromOnnxPath,
-    SaveEngine,
-    TrtRunner,
 )
 
 
@@ -15,13 +13,15 @@ from polygraphy.backend.trt import (
 @click.option("--fp16", is_flag=True, help="Enable FP16 precision")
 def build_tensorrt_engine(onnx_path: Path, fp16: bool):
     """Build and save a TensorRT engine from an ONNX model."""
-    #check if file exists
+    # check if file exists
     if not onnx_path.exists():
         click.echo(f"File {onnx_path} does not exist")
         return
-    build_engine = EngineFromNetwork(NetworkFromOnnxPath(str(onnx_path)), config=CreateConfig(fp16=fp16))
-    SaveEngine(build_engine, str(onnx_path.with_suffix(".engine")))
-    TrtRunner(build_engine).activate()
+    network_loader = NetworkFromOnnxPath(str(onnx_path))
+    engine_loader = EngineFromNetwork(network_loader, config=CreateConfig(fp16=fp16))
+    engine = engine_loader()
+    with open(onnx_path.with_suffix(".engine"), "wb") as f:
+        f.write(engine.serialize())
     click.echo(f"Engine saved at {onnx_path.with_suffix('.engine')}")
 
 
